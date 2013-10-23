4c0e8ac6d9f08211a6d1c29c9e488721a9c8b647
qujinlong <qujinlong@ruyicai.com>
2013-10-23 16:05:30 +0800

    更换力美的ip

--- src/main/java/com/ruyicai/advert/util/VerifyUtil.java
+++ src/main/java/com/ruyicai/advert/util/VerifyUtil.java
@@ -1,6 +1,6 @@
 package com.ruyicai.advert.util;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang.StringUtils;
 
@@ -16,11 +16,27 @@ public class VerifyUtil {
 		if (StringUtils.isBlank(ip)) {
 			return false;
 		}
+		List<String> rightIpList = new ArrayList<String>();
+		String[] split = StringUtils.split(rightIp, ",");
+		for (String string : split) {
+			if (StringUtils.isBlank(string)) {
+				continue;
+			}
+			if (StringUtils.indexOf(string, "-")>-1) {
+				String[] split2 = StringUtils.split(string, "-");
+				String prefix = StringUtils.substringBeforeLast(split2[0], ".");
+				String startIndex = StringUtils.substringAfterLast(split2[0], ".");
+				String endIndex = StringUtils.substringAfterLast(split2[1], ".");
+				for (int i = Integer.valueOf(startIndex); i <= Integer.valueOf(endIndex); i++) {
+					rightIpList.add(prefix+"."+i);
+				}
+			} else {
+				rightIpList.add(string);
+			}
+		}
 		String[] ips = StringUtils.split(ip, ",");
-		String[] limeiIps = StringUtils.split(rightIp, ",");
-		List<String> limeiIpList = Arrays.asList(limeiIps);
 		for (String p : ips) {
-			if (StringUtils.isNotBlank(p)&&limeiIpList.contains(p.trim())) {
+			if (StringUtils.isNotBlank(p)&&rightIpList.contains(p.trim())) {
 				return true;
 			}
 		}
